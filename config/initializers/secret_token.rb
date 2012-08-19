2dc45da9d6d7412b138872b8eebb49c41fb8fdda
An Yu <an@indiegogo.com>
2012-08-19 01:26:38 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '3073bf6300a75c074e0d316594ac6ded529799687cfc7b73c5f41f5e110c09211b6d644290679506e1f945f24a4887ce7cbad7976165833287e8ef7e5909795a'
