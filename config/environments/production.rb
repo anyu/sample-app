832114ec8534be292bc1ec761d384482971d0834
An Yu <an@indiegogo.com>
2012-08-25 21:04:17 -0700

    Add SSL in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,11 @@ SampleApp::Application.configure do
   config.assets.compile = false
 
   # Generate digests for assets URLs
-  config.assets.digest = true
+  config.assets.digest = 
+
+  # Force all access to the app over SSL, use Strict-Transport-Security, 
+  # and use secure cookies.
+  config.force_ssl = true
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
